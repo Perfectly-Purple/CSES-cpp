93ad21c995aec8ced339b80327e6d9c790100665
Surabhi <86736506+Perfectly-Purple@users.noreply.github.com>
2021-07-04 01:45:50 +0530

    Add files via upload

---
+++ MinMoves.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n; long moves=0;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++)
+		cin>>arr[i];
+	
+	for(int i=1; i<n; i++)
+	{
+		if(arr[i]<arr[i-1])
+		{
+			moves+=arr[i-1]-arr[i];
+			arr[i]=arr[i-1];
+		}
+	}
+	cout<<moves;
+}
